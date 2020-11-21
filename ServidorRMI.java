dd406ba865d0aedadaf63546f76de013a50629da
Ubuntu <manbenit@nodo0.frkybqr02x3evaiwgto1wb00dc.jx.internal.cloudapp.net>
2020-11-21 06:25:06 +0000

    sdasd

--- ServidorRMI.java
+++ ServidorRMI.java
@@ -9,7 +9,7 @@ public class ServidorRMI{
         
         String url = "rmi://localhost/matrices";
         int N= Integer.parseInt(angrs[0]);
-        ClaseRMI obj = new ClaseRMI();
+        ClaseRMI obj = new ClaseRMI(N);
 
 
         // registra la instancia en el rmiregistry
